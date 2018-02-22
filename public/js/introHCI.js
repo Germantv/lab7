3363d1d211329a64cd3062dd22a9a9a8e38d4492
German Flores <gfloresa@ucsd.edu>
2018-02-22 10:52:41 -0800

    google analytics A/B testing

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,8 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$('.likeBtn').click(function(e){
+		e.preventDefault();
+		console.log("clicked");
+		ga("send", "event", 'like', 'click')});
 }
